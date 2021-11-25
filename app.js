922c2899effac7920d65c0d1dbe5433148dc0682
Bernardanxiety <beniek12@onet.pl>
2021-11-25 11:13:29 -0800

    redoing whole thing

--- app.js
+++ app.js
@@ -2,27 +2,9 @@ const grid = document.querySelector('.grid');
 const parentWidth = grid.offsetWidth;
 const parentHeight = grid.offsetHeight;
 const numberOfGrids = 16;
-const numberOfDivs = numberOfGrids*numberOfGrids;
-const divs = document.querySelectorAll('div');
+const clear = document.querySelector('.clear');
 
 
-grid.setAttribute(`style`, `display: grid; grid-template-rows: repeat(${numberOfGrids}, 1fr);
-grid-template-columns: repeat(${numberOfGrids}, 1fr)`);
-
-
-
-
-
-for (let i=0; i<numberOfDivs; i++) {
-    const gridCard = document.createElement('div');
-    grid.appendChild(gridCard);
-}
-
-divs.forEach((div) => div.addEventListener('mouseover', (e) => {
-    let randomColor = Math.floor(Math.random()*100 +1);
-    let randomHsl = Math.floor(Math.random()*360 + 1);
-    e.target.setAttribute(`style`, `background-color: hsl(${randomHsl}, ${randomColor}%, ${randomColor}%)`);
-}))
 
 
 

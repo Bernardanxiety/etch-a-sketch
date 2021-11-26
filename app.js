519cea9d3e04da4475317c22c7ff7efcc5e0498e
Bernardanxiety <beniek12@onet.pl>
2021-11-26 02:20:41 -0800

    Redoing menu buttons and add a bg function

--- app.js
+++ app.js
@@ -7,15 +7,13 @@ const div = document.createElement('div');
 const gridInner = document.querySelectorAll('.gridInner');
 const colorBtns = document.querySelectorAll('.color');
 let currentSize = numberOfGrids;
-let color = 'black';
+let color = 'hsl(0, 0%, 0%)';
 
 colorBtns.forEach((colorBtn) => colorBtn.addEventListener('click', (e) => {
-    colorBtns.forEach((colorBtn) => colorBtn.removeAttribute('style', 'background-color: red'));
-    // e.target.setAttribute('style', 'background-color: red');
-    let activeColor = e.target.textContent;
-    addBG(activeColor);
-    e.target.setAttribute('style', `background-color: ${color}`);
-}))
+    console.log(e.target.textContent);
+    
+    
+}));
 
 
 
@@ -33,41 +31,10 @@ function fillGrid() {
 
 }
 
-function addBG(activeColor) {
-    const gridInner = document.querySelectorAll('.gridInner');
-    let currentColor;
-    if(activeColor==='Random') {
-        function randomColor() {
-            let randomNumber = Math.floor(Math.random()*360 + 1);
-        let randomPercent = Math.floor(Math.random()*100 + 1);
-        let anotherPercent = Math.floor(Math.random()*100+1);
-        currentColor = `hsl(${randomNumber}, ${randomPercent}%, ${anotherPercent}%)`;
-        return currentColor;
-        }
-        currentColor = randomColor();
-        console.log(currentColor);
-    }
-    else {
-        currentColor = activeColor;
-    }
-    gridInner.forEach((gridInner) => gridInner.addEventListener('mouseover', (e) => {
-        e.target.setAttribute('style', `background-color: ${currentColor}`);
-    }))
 
-    return color = currentColor;
-}
 
 clear.addEventListener('click', () => {
-    const gridInner = document.querySelectorAll('.gridInner');
-    gridInner.forEach((gridInner) => gridInner.removeAttribute('style'));
-    currentSize = Number(prompt('How many rows and columns do you want? Capped at 100'));
-    if(currentSize>100) {
-        currentSize = 100;
-        alert('Can\'t be higher than 100!');
-    }
-    else if(currentSize===0) {
-        currentSize = 16;
-    }
+    
 
     setGrid(currentSize);
     fillGrid();
